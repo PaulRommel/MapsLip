482af060a0a5dd449283cddbff11c526fc9a9681
Paul <paulrommel87@gmail.com>
2022-11-05 10:45:52 +0300

    Initial Commit

---
+++ MapsLip/MapsLipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsLipApp.swift
+//  MapsLip
+//
+//  Created by Павел Попов on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapsLipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
